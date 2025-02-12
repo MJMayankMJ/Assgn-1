e846989683d66b4d45265e0bfa9c814e2e40b411
Mayank Jangid <226301131@gkv.ac.in>
2025-02-12 16:50:44 +0530

    Initial Commit

---
+++ Assgn 1Tests/Assgn_1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Assgn_1Tests.swift
+//  Assgn 1Tests
+//
+//  Created by Mayank Jangid on 2/12/25.
+//
+
+import Testing
+@testable import Assgn_1
+
+struct Assgn_1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Assgn 1UITests/Assgn_1UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Assgn_1UITestsLaunchTests.swift
+//  Assgn 1UITests
+//
+//  Created by Mayank Jangid on 2/12/25.
+//
+
+import XCTest
+
+final class Assgn_1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
